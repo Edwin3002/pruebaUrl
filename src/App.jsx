95f426a2e4bddc6b06f5eec962dd7489d104d08a
Edwin Vargas <edwin.vargas@gse.com.co>
2024-04-06 11:05:46 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const redirectUrl = () => {
+    const url =
+      "intent://instagram.com/#Intent;scheme=https;package=com.instagram.android;end";
+
+    // window.location.replace(url);
+    setCount((count) => count+1)
+    window.location.replace("animations1://path/"); 
+  }
+  const inst = () => {
+    const url =
+      "intent://instagram.com/#Intent;scheme=https;package=com.instagram.android;end";
+
+    // window.location.replace(url);
+    window.location.replace(url); 
+  }
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <button onClick={redirectUrl}>
+          redirect
+        </button>
+        <button onClick={inst}>
+          inst
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
